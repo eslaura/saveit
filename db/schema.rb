76f8f2f7dcdbec519fb94df52d5c2c4481832820
laura hallam <laura_hallam@yahoo.co.uk>
2017-12-12 12:36:00 +0100

    added components to file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212102621) do
+ActiveRecord::Schema.define(version: 20171212110258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.string   "url"
+    t.string   "color"
+    t.string   "original_store"
+    t.integer  "size"
+    t.integer  "price"
+    t.integer  "user_id"
+    t.boolean  "favorite"
+    t.integer  "user_price"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+    t.index ["user_id"], name: "index_items_on_user_id", using: :btree
+  end
+
+  create_table "registrations", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_registrations_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_registrations_on_reset_password_token", unique: true, using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -32,4 +65,5 @@ ActiveRecord::Schema.define(version: 20171212102621) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "items", "users"
 end
