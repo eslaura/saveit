727693f0a946a51a5db090e75ae8bfd59eae7869
Yuzhu Hou <yuzhu.hou@gmail.com>
2017-12-12 12:07:46 +0100

    set all variables for the item model-without linking the price model

---
+++ db/migrate/20171212110258_create_items.rb
@@ -0,0 +1,18 @@
+class CreateItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :items do |t|
+      t.string :name
+      t.string :description
+      t.string :url
+      t.string :color
+      t.string :original_store
+      t.integer :size
+      t.integer :price
+      t.references :user, foreign_key: true
+      t.boolen :favorite
+      t.integer :user_price
+
+      t.timestamps
+    end
+  end
+end
