00c938b908128a64d00f770a09ac5f89104a9e39
Andrea dAlessandro <dalexandrea@gmail.com>
2017-12-14 11:44:06 +0100

    Modal Card effect on click

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,7 @@
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+//= require jquery
+//= require bootstrap-sprockets
+//= require rails-ujs
 //= require_tree .
