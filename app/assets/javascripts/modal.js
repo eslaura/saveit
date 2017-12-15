40039cf2be33b20099e3a9e47b6cfcdcac5c4789
Yuzhu Hou <yuzhu.hou@gmail.com>
2017-12-15 11:09:35 +0100

    completed module

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,40 +1,40 @@
-// Get the modal
-var modal = document.getElementsByClassName('myModal');
-// Get the button that opens the modal
-var btns = document.getElementsByClassName("trigger");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-console.log(btns)
+// // Get the modal
+// var modal = document.getElementsByClassName('myModal');
+// // Get the button that opens the modal
+// var btns = document.getElementsByClassName("trigger");
+// // Get the <span> element that closes the modal
+// var span = document.getElementsByClassName("close")[0];
+// console.log(btns)
 
-for(btn of btns) {
-  console.log(btn)
-  const modalId = event.target.getAttribute("data-id")
-  // console.log(btn.)
-}
-// When the user clicks the button, open the modal
 // for(btn of btns) {
-//  btn.onclick = function(event) {
-//     const modalId = event.target.getAttribute("data-id")
-//     where modalId: modalId do
-//     modal.style.display = "block";
-//   }
+//   console.log(btn)
+//   const modalId = event.target.getAttribute("data-id")
+//   // console.log(btn.)
+// }
+// // When the user clicks the button, open the modal
+// // for(btn of btns) {
+// //  btn.onclick = function(event) {
+// //     const modalId = event.target.getAttribute("data-id")
+// //     where modalId: modalId do
+// //     modal.style.display = "block";
+// //   }
+// // }
+
+// btns.forEach(modalIteration())
+
+// function modalIteration(event) {
+//   const modalId = event.target.getAttribute("data-id")
 // }
 
-btns.forEach(modalIteration())
 
-function modalIteration(event) {
-  const modalId = event.target.getAttribute("data-id")
-}
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
 
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
