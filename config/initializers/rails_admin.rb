855d7d4f6c316e0b2dfe54872ad52f9f9af310dc
laura hallam <laura_hallam@yahoo.co.uk>
2017-12-18 15:53:30 +0100

    in admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,10 +1,5 @@
 RailsAdmin.config do |config|
 
-  config.authorize_with do |controller|
-    redirect_to main_app.root_path unless current_user && current_user.admin
-  end
-
-
   ### Popular gems integration
 
   ## == Devise ==
